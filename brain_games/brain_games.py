de833dd6a45c682fbad4768c9ba54872b9fb34f2
Vol'nov Kirill <volnov-93@mail.ru>
2023-01-14 22:32:57 +0300

    cli.py

---
+++ brain_games/brain_games.py
@@ -0,0 +1,4 @@
+from cli import welcome_user
+def function():
+    x = welcome_user()
+    print('Hello, "x"!')
---
+++ brain_games/cli.py
@@ -0,0 +1,4 @@
+welcome_user:
+	import prompt
+
+name = prompt.string('May I have your name? ')
