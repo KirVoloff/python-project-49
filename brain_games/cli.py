012270547b0098bbc6b246a72e33413e74b79c2b
Vol'nov Kirill <volnov-93@mail.ru>
2023-01-18 13:28:17 +0300

    brain_games.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,8 @@
-import prompt
+#!/usr/bin/env python3
 def main():
-    print("Welcome to the Brain Games!")
-  if __name__ == '__main__':
-    main()
-
+  print("Welcome to the Brain Games!")
+if __name__ == '__main__':
+  main()
+import prompt
 name = prompt.string('May I have your name? ')
 print("Hello,", name, "!")
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,3 @@
 #!/usr/bin/env python3
-from cli import name
-
-
-def main():
-    print("Welcome to the Brain Games!")
-
-
-if __name__ == '__main__':
-    main()
-
-
-def call():
-    call.name()
+import cli
+cli.name()
