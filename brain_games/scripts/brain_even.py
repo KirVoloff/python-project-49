6d8d2aa4a42d522c9e1163c0c3aa9355c95cc3a9
Vol'nov Kirill <volnov-93@mail.ru>
2023-02-01 20:18:13 +0300

    brain_games/__pycache__/cli.cpython-38.pyc$

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python3
 import prompt
-
-
 def welcome_user():
     print('Welcome to the Brain Games!')
-    global name
     name = prompt.string('May I have your name? ')
     print('Hello,' + name + "!")
