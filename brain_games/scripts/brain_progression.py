7bec8a9e5e3c66854d40e9d7e5941fc7fab9dc26
Vol'nov Kirill <volnov-93@mail.ru>
2023-02-08 23:01:07 +0300

    brain_games/scripts/brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome
+from brain_games.mygame import game_progression
+
+
+def main():
+    name = welcome()
+    round = 0
+    print('What number is missing in the progression?')
+    while round != 3:
+        answer, element = game_progression()
+        if answer == element:
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong ;(. Correct answer was '{element}'")
+            print(f"Let's try again, {name}!")
+            break
+        round += 1
+        if round == 3:
+            print(f"Congratulations, {name}! ")
+
+
+
+if __name__ == '__main__':
+    main()
