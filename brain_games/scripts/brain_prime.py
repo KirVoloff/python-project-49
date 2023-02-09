a275dc4dfff714af51aa379e9ddd36b1cb1a9976
Vol'nov Kirill <volnov-93@mail.ru>
2023-02-09 18:38:29 +0300

    brain_games/scripts/brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome
+from brain_games.mygame import game_prime
+
+
+def main():
+    name = welcome()
+    round = 0
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    while round != 3:
+        answer, True_answer = game_prime()
+        if answer == True_answer:
+            print('Corret!')
+        else:
+            print(f"Let's try again, {name}!")
+            break
+        round += 1
+        if round == 3:
+            print(f"Congratulations, {name}! ")
+
+
+if __name__ == '__main__':
+    main()
