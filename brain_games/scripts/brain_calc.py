256db69b903d50a94fd9058eb9d5e19081eeafe3
Vol'nov Kirill <volnov-93@mail.ru>
2023-02-13 00:21:48 +0300

    brain_games/games/

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,36 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome
-import random
+from brain_games.game_starter import start_game
+from brain_games.games import calc
 
 
 def main():
-    name = welcome()
-    round = 0
-    print('What is the result of the expression?')
-    while round != 3:
-        number_1 = random.randint(0, 100)
-        number_2 = random.randint(0, 100)
-        math_operation = random.choice(['+', '*', '-'])
-        Question = str(number_1) + ' ' + math_operation + ' ' + str(number_2)
-
-        def variation_1():
-            variation = {
-                math_operation == "+": number_1 + number_2,
-                math_operation == "-": number_1 - number_2,
-                math_operation == "*": number_1 * number_2
-            }
-            return variation[True]
-        print(f"Question: {Question}")
-        answer = int(input('Your answer: '))
-        if answer == variation_1():
-            print("Correct")
-        else:
-            print(f"'{answer}' is wrong ;(. Correct answer was '{variation_1()}'")
-            print(f"Let's try again, {name}!")
-            break
-        round += 1
-        if round == 3:
-            print(f"Congratulations, {name}! ")
+    start_game(calc)
 
 
 if __name__ == '__main__':
